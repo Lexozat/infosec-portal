e27ebcc1bc9e79aa2cf6c98c740a9f4524d8cd55
Lexozat <azatiashvili@gmail.com>
2025-03-25 15:06:26 +0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
+    domains: ['images.unsplash.com', 'source.unsplash.com', 'ext.same-assets.com', 'ugc.same-assets.com'],
   },
   typescript: {
+    // Skip type checking during build (since we have some type errors)
     ignoreBuildErrors: true,
   },
   eslint: {
+    // Skip ESLint during builds
     ignoreDuringBuilds: true,
   },
+  swcMinify: true,
 };
 
 export default nextConfig;
